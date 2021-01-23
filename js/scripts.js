c62ef9323d33529f6a829b653ba86e0f1ea97853
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-01-22 18:49:04 -0800

    Fix optional name related issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // This is the business (or back-end) logic:
-let worker = function (inputInteger, orderCB) {
+let worker = function (inputInteger, orderCB, subName) {
   let generatedIntegers = [];
   let manipulatedIntegers = [];
   generatedIntegers.splice(0, generatedIntegers.length);
   manipulatedIntegers.splice(0, manipulatedIntegers.length);
-  // alert("size of real and man " + generatedIntegers.length + ":" + manipulatedIntegers.length);
-  const replacementString = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+  const thirdMessage = (subName === "") ? "Won't you be my neighbor?" : "Won't you be my neighbor, " +subName +"?";
+  const replacementString = ["Beep!", "Boop!", thirdMessage];
   const keyNumbers = [1, 2, 3];
   const rExpresssion = /[1-3]/g;  //catch digit '1' or '2' or '3' or various permutation of three
   for (let i = 0; i <= inputInteger; i++) {
@@ -29,14 +29,9 @@ let worker = function (inputInteger, orderCB) {
   });
 
   if (orderCB) {
-    alert("Inside orderCB yes loop");
-    // alert("manipulated array is " + manipulatedIntegers.reverse());
-    // let manipulatedIntegers2 = [];
-    const manipulatedIntegers2 = manipulatedIntegers.reverse().slice()
-    return manipulatedIntegers2;
+    return manipulatedIntegers.reverse().slice();
   }
   else {
-    // alert("inside orderCB no loop");
     return manipulatedIntegers;
   }
 }
@@ -46,37 +41,18 @@ let worker = function (inputInteger, orderCB) {
 $(document).ready(function () {
   $("#entry").submit(function (e) {
     $("#story").hide();
-    // $("#story").addClass("storyClass");
     var cBClicked = false;
     let inputNum = parseInt($("input#numeral").val());
     $('input[type="number"], textarea').val('');  // to clear form of entered value after submit
+    let subName = $("input#subName").val();
+    $('input[type="text"], textarea').val('');  // to clear form of entered value after submit
     $("input:checkbox[name=order]:checked").each(function () {
       cBClicked = true;
       $("#cb").prop('checked', false); // to clear form of entered value after submit  
     });
-    let substituteName;
-    // $("input:checkbox[name=substituteBox]:checked").each(function () {
-    //   substituteName = $('#substituteName').val();
-    //   alert("substituteName is " + substituteName);
-    //   $('input[type="text"], textarea').val('');  // to clear form of entered value after submit  
-    //   cBClicked = true;
-    // });
-
-    $("input:checkbox[name=substituteBox]:checked").each(function () {
-      substituteName = $('#substituteName').val();
-      $('input[type="text"], textarea').val('');  // to clear form of entered value after submit  
-      cBClicked = true;
-      alert("substituteName is " + substituteName);
-    });
-
-
     let finalIntegerArray = [];
     finalIntegerArray.splice(0, finalIntegerArray.length);
-    // alert("size of finalIntegerArray " + finalIntegerArray.length);    
-    finalIntegerArray = worker(inputNum, cBClicked);
-    // alert("finalIntegerArray array is " + finalIntegerArray);
-
-    // $("#story").empty().show();
+    finalIntegerArray = worker(inputNum, cBClicked, subName);
     $("#story").show();
     $(".inputNumber").text(inputNum.toString());
     let k = -1;
@@ -90,10 +66,12 @@ $(document).ready(function () {
       step = -1;
     }
     $("ul#answer").empty();
+    $("ul#question").empty();
     finalIntegerArray.forEach(function (element) {
-
-      let num = "<li>Numeral " + k + " <strong>" + element + "</strong></li>";
-      $("ul#answer").append(num);
+      let textQuestion = "<li>Numeral " + k + "</li>";
+      let textAnswer = "<li><strong>" + element + "</strong></li>";
+      $("ul#question").append(textQuestion);
+      $("ul#answer").append(textAnswer);
       k = k + step;
     });
     e.preventDefault();
