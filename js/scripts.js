87380b8c28b5e55f6e5bc55d8c09e0aabdfbee46
svayalk <32828154+Rekjal@users.noreply.github.com>
2021-01-22 17:52:00 -0800

    Add new input field to capture Optional Name

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@
 let worker = function (inputInteger, orderCB) {
   let generatedIntegers = [];
   let manipulatedIntegers = [];
-  generatedIntegers.splice(0,generatedIntegers.length);
-  manipulatedIntegers.splice(0,manipulatedIntegers.length);
+  generatedIntegers.splice(0, generatedIntegers.length);
+  manipulatedIntegers.splice(0, manipulatedIntegers.length);
   // alert("size of real and man " + generatedIntegers.length + ":" + manipulatedIntegers.length);
   const replacementString = ["Beep!", "Boop!", "Won't you be my neighbor?"];
   const keyNumbers = [1, 2, 3];
@@ -27,15 +27,15 @@ let worker = function (inputInteger, orderCB) {
       manipulatedIntegers.push(generatedInteger.toString());
     }
   });
-  
-  if (orderCB)  {
+
+  if (orderCB) {
     alert("Inside orderCB yes loop");
     // alert("manipulated array is " + manipulatedIntegers.reverse());
     // let manipulatedIntegers2 = [];
-    const manipulatedIntegers2 = manipulatedIntegers.reverse().slice()  
+    const manipulatedIntegers2 = manipulatedIntegers.reverse().slice()
     return manipulatedIntegers2;
   }
-  else  {
+  else {
     // alert("inside orderCB no loop");
     return manipulatedIntegers;
   }
@@ -50,36 +50,51 @@ $(document).ready(function () {
     var cBClicked = false;
     let inputNum = parseInt($("input#numeral").val());
     $('input[type="number"], textarea').val('');  // to clear form of entered value after submit
-    $("input:checkbox[name=order]:checked").each(function() {
-     $('input[type="text"], textarea').val('');  // to clear form of entered value after submit  
-     cBClicked = true;;
+    $("input:checkbox[name=order]:checked").each(function () {
+      cBClicked = true;
+      $("#cb").prop('checked', false); // to clear form of entered value after submit  
     });
-    
+    let substituteName;
+    // $("input:checkbox[name=substituteBox]:checked").each(function () {
+    //   substituteName = $('#substituteName').val();
+    //   alert("substituteName is " + substituteName);
+    //   $('input[type="text"], textarea').val('');  // to clear form of entered value after submit  
+    //   cBClicked = true;
+    // });
+
+    $("input:checkbox[name=substituteBox]:checked").each(function () {
+      substituteName = $('#substituteName').val();
+      $('input[type="text"], textarea').val('');  // to clear form of entered value after submit  
+      cBClicked = true;
+      alert("substituteName is " + substituteName);
+    });
+
+
     let finalIntegerArray = [];
-    finalIntegerArray.splice(0,finalIntegerArray.length);
+    finalIntegerArray.splice(0, finalIntegerArray.length);
     // alert("size of finalIntegerArray " + finalIntegerArray.length);    
     finalIntegerArray = worker(inputNum, cBClicked);
     // alert("finalIntegerArray array is " + finalIntegerArray);
-    
+
     // $("#story").empty().show();
     $("#story").show();
     $(".inputNumber").text(inputNum.toString());
     let k = -1;
-    let step = 0; 
-    if(cBClicked === false) {
+    let step = 0;
+    if (cBClicked === false) {
       k = 0;
       step = 1;
     }
     else {
       k = inputNum;
-      step= -1;
+      step = -1;
     }
     $("ul#answer").empty();
     finalIntegerArray.forEach(function (element) {
-      
+
       let num = "<li>Numeral " + k + " <strong>" + element + "</strong></li>";
       $("ul#answer").append(num);
-      k = k +step;
+      k = k + step;
     });
     e.preventDefault();
   });
